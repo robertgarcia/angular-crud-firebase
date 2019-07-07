b1e8503268dd3bdb648478fe573c5f7aa0094248
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-07-07 00:47:03 -0600

    Diseño de la página de héroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
-  { path: 'heroe', component: HeroeComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
 ];
 
