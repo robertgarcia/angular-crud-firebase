28187ee5eda0c02f81705f8c918213d1db8891c0
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-07-12 15:01:52 -0600

    HTTP - GET Obtener un listado todos los héroes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HeroeModel } from '../models/heroe.model';
 import { map } from 'rxjs/operators';
+import { Key } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +29,24 @@ export class HeroesService {
     };
     delete heroeTemp.id;
     return this.http.put(`${ this.url }/heroes/${ heroe.id }.json`, heroeTemp);
+  }
+
+  getHeroes(){
+    return this.http.get(`${ this.url }/heroes.json`).pipe(
+      map( this.crearArreglo )
+    );
+  }
+
+  private crearArreglo(heroesObj: object){
+    if(heroesObj === null ){
+      return [];
+    }
+    const heroes: HeroeModel[] = [];
+    Object.keys( heroesObj ).forEach( key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+      heroes.push(heroe);
+    });
+    return heroes;
   }
 }
